3b427541756361849f918972ac638aad665b5b6f
PedroHAVeloso <contatopedrohalves@gmail.com>
2023-03-08 18:03:44 -0300

    Adição da OnAccess
    Trabalho de final de ano
    OnAccess

---
+++ onaccess/lib/pages/login/login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          "Olá",
+          style: TextStyle(fontSize: 30),
+        )
+      ],
+    );
+  }
+}
