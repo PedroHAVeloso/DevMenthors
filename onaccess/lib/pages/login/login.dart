f8bb88f96aafab447c1ee44412d0b4e97aee5fbb
Pedro Henrique Alves Veloso <contatopedrohalves@gmail.com>
2023-03-11 11:53:39 -0300

    Adição Tela Login / BtnVoltar
    Adicionada a tela de login, o botão de voltar.
    Na tela de login adicionado a logo, o botão e dois TextView.

--- onaccess/lib/pages/login/login.dart
+++ onaccess/lib/pages/login/login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:onaccess/assets/color/paleta_cores.dart';
+import 'package:onaccess/components/buttons/btn_voltar.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -13,15 +15,104 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(
-          "Olá",
-          style: TextStyle(fontSize: 30),
-        )
-      ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const BtnVoltar(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 55,
+                  width: 160,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("lib/assets/images/OnAccess.png"),
+                      fit: BoxFit.scaleDown,
+                    ),
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Text(
+                "CPF",
+                style: TextStyle(
+                  color: PaletaCores.cinza01(),
+                  fontSize: 25,
+                  fontFamily: 'Baloo',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 280,
+                  height: 45,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(100),
+                        ),
+                      ),
+                      filled: true,
+                      fillColor: PaletaCores.cinza04(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Text(
+                "Senha",
+                style: TextStyle(
+                  color: PaletaCores.cinza01(),
+                  fontSize: 25,
+                  fontFamily: 'Baloo',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 280,
+                  height: 45,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(100),
+                        ),
+                      ),
+                      filled: true,
+                      fillColor: PaletaCores.cinza04(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
